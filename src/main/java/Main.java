f02882db2797c93c7b9a74b40a8e8757af242d32
Timothy <timothy@Zipcoders-MBP-3.fios-router.home>
2023-03-01 23:36:39 -0500

    i have made changes tp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
 public class Main {
 
     public static void main(String[] args ){
 
+        System.out.println("Hi, what is your name? ");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.next();
+
+        if (userName.equalsIgnoreCase("Alice") || userName.equalsIgnoreCase("Bob")) {
+            System.out.println("Nice you meet you " + userName + "!");
+        } else {
+            System.out.println("Wierd name....");
+        }
     }
 }
